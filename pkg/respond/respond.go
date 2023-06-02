0c8238430da7ddb8d406d5de91d8ec01a565d19f
Suneel Freimuth <suneelfreimuth1@gmail.com>
2023-06-02 14:11:12 -0700

    Problem details

--- pkg/respond/respond.go
+++ pkg/respond/respond.go
@@ -2,160 +2,53 @@ package respond
 
 import (
 	"encoding/json"
-	// "encoding/xml"
 	"fmt"
 	"net/http"
 )
 
-const (
-	EncodeJSON = 0
-	EncodeXML = 1
-)
-
-// type Problem interface {
-// 	GetType() string
-// 	GetTitle() string
-// 	GetStatus() int
-// 	GetDetail() string
-// 	GetInstance() string
-// }
-
-// A problem details object conforming to RFC 7807 §3.1.
-// https://datatracker.ietf.org/doc/html/rfc7807#section-3.1
-// 
-// TODO: Examples.
-type Problem struct {
-	Type string `json:"type,omitempty"`
-	Title string `json:"title,omitempty"`
-	Status int `json:"status,omitempty"`
-	Detail string `json:"detail,omitempty"`
-	Instance string `json:"instance,omitempty"`
+type Problem interface {
+	GetStatus() int
 }
 
-// func (bp BaseProblem) GetType() string {
-// 	return bp.Type
-// }
+// The minimum implementation of a problem details object conforming to RFC 7807.
+//
+// To define a problem details object with extensions, embed BaseProblem:
+// 
+//  type purchaseProblem struct {
+//      respond.BaseProblem
+//      Balance  int      `json:"balance,omitempty"`
+//      Accounts []string `json:"accounts,omitempty"`
+//  }
+type BaseProblem struct {
+	Type     string `json:"type,omitempty" xml:"type,omitempty"`
+	Title    string `json:"title,omitempty" xml:"title,omitempty"`
+	Status   int    `json:"status,omitempty" xml:"status,omitempty"`
+	Detail   string `json:"detail,omitempty" xml:"detail,omitempty"`
+	Instance string `json:"instance,omitempty" xml:"instance,omitempty"`
+}
 
-// func (bp BaseProblem) GetTitle() string {
-// 	return bp.Title
-// }
+func (bp BaseProblem) GetStatus() int {
+	return bp.Status
+}
 
-// func (bp BaseProblem) GetStatus() int {
-// 	return bp.Status
-// }
-
-// func (bp BaseProblem) GetDetail() string {
-// 	return bp.Detail
-// }
-
-// func (bp BaseProblem) GetInstance() string {
-// 	return bp.Instance
-// }
-
-// func (p Problem) MarshalJSON() ([]byte, error) {
-// 	buf := []byte{ '{' }
-// 	if p.Type != "" {
-// 		buf = append(buf, fmt.Sprintf(`"type":"%s",`, p.Type)...)
-// 	}
-// 	if p.Title != "" {
-// 		buf = append(buf, fmt.Sprintf(`"title":"%s",`, p.Title)...)
-// 	}
-// 	if p.Status != 0 {
-// 		buf = append(buf, fmt.Sprintf(`"status":%d,`, p.Status)...)
-// 	}
-// 	if p.Detail != "" {
-// 		buf = append(buf, fmt.Sprintf(`"detail":"%s",`, p.Detail)...)
-// 	}
-// 	if p.Instance != "" {
-// 		buf = append(buf, fmt.Sprintf(`"instance":"%s",`, p.Instance)...)
-// 	}
-
-// 	extensions, err := json.Marshal(p.Extensions)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	if buf[len(buf)-1] == ',' {
-// 		buf[len(buf)-1] = '}'
-// 	} else {
-// 		buf = append(buf, '}')
-// 	}
-// 	return buf, nil
-// }
-
-// func isJSONObject(bs []byte) bool {
-	
-// }
-
-// func (p *Problem) UnmarshalJSON(data []byte) error {
-// 	var entries map[string]any
-// 	err := json.Unmarshal(data, &entries)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	var ok bool
-// 	for k, v := range entries {
-// 		switch k {
-// 		case "type":
-// 			p.Type, ok = v.(string)
-// 			if !ok {
-// 				return fmt.Errorf(`expected "type" to be a string, got %v`, v)
-// 			}
-// 		case "title":
-// 			p.Title, ok = v.(string)
-// 			if !ok {
-// 				return fmt.Errorf(`expected "title" to be a string, got %v`, v)
-// 			}
-// 		case "status":
-// 			status, ok := v.(float64)
-// 			if !ok {
-// 				return fmt.Errorf(`expected "status" to be a float64, got %v`, v)
-// 			}
-
-// 			p.Status = int(status)
-// 		case "detail":
-// 			p.Detail, ok = v.(string)
-// 			if !ok {
-// 				return fmt.Errorf(`expected "detail" to be a string, got %v`, v)
-// 			}
-// 		case "instance":
-// 			p.Instance, ok = v.(string)
-// 			if !ok {
-// 				return fmt.Errorf(`expected "instance" to be a string, got %v`, v)
-// 			}
-// 		default:
-// 			if p.Extensions == nil {
-// 				p.Extensions = make(map[string]any)
-// 			}
-// 			p.Extensions[k] = v
-// 		}
-// 	}
-// 	return nil
-// }
-
-func WithProblem(w http.ResponseWriter, enc int, p Problem) error {
+// Write an HTTP response with the JSON encoding of the problem as the response body.
+//
+// The status code will be taken from the problem. If absent, it will be 400 Bad Request.
+func WithProblem(w http.ResponseWriter, p Problem) error {
 	var bs []byte
 	var err error
-	switch enc {
-	case EncodeJSON:
-		w.Header().Set("Content-Type", "application/problem+json")
-		bs, err = json.Marshal(p)
-		if err != nil {
-			return fmt.Errorf("failed to encode Problem as JSON: %v", err)
-		}
-	case EncodeXML:
-		w.Header().Set("Content-Type", "application/problem+xml")
-		panic("TODO")
-	default:
-		return fmt.Errorf("unknown encoding supplied, use EncodeJSON or EncodeXML")
+	w.Header().Set("Content-Type", "application/problem+json")
+	bs, err = json.Marshal(p)
+	if err != nil {
+		return fmt.Errorf("failed to encode Problem as JSON: %v", err)
 	}
-	
+
 	if s := p.GetStatus(); s != 0 {
 		w.WriteHeader(s)
 	} else {
 		w.WriteHeader(400)
 	}
+
 	_, err = w.Write(bs)
 	if err != nil {
 		return err
--- pkg/respond/respond_test.go
+++ pkg/respond/respond_test.go
@@ -3,7 +3,9 @@ package respond
 import (
 	"encoding/json"
 	"fmt"
+	"net/http"
 	"reflect"
+	"strings"
 	"testing"
 )
 
@@ -16,14 +18,14 @@ type purchaseProblem struct {
 
 
 func TestProblemToJSON(t *testing.T) {
-	t.Run("empty problem -> empty object", func(t *testing.T) {
-		p := BaseProblem{}
+	t.Run("empty problem -> only extensions", func(t *testing.T) {
+		p := purchaseProblem{}
 		bs, err := json.Marshal(p)
 		if err != nil {
 			t.Error("error while encoding Problem as JSON:", err)
 		}
-		if !reflect.DeepEqual(bs, []byte("{}")) {
-			t.Errorf("empty problem did not encode as empty object, got %s", bs)
+		if !reflect.DeepEqual(bs, []byte(`{"balance":0,"accounts":null}`)) {
+			t.Errorf("empty problem did not only encode the extensions, got %s", bs)
 		}
 	})
 
@@ -61,7 +63,7 @@ func TestProblemToJSON(t *testing.T) {
 
 func TestProblemFromJSON(t *testing.T) {
 	t.Run("empty JSON object -> empty Problem", func (t *testing.T)  {
-		var p Problem
+		var p purchaseProblem
 		err := json.Unmarshal([]byte("{}"), &p)
 		if err != nil {
 			t.Errorf("error while unmarshaling JSON, %v", err)
@@ -71,7 +73,7 @@ func TestProblemFromJSON(t *testing.T) {
 		}
 	})
 	t.Run("complete JSON object -> complete Problem", func (t *testing.T)  {
-		var p Problem
+		var p purchaseProblem
 		err := json.Unmarshal([]byte(`{
 			"type": "https://example.io/problems/out-of-credit",
 			"title": "You do not have enough credit.",
@@ -110,4 +112,56 @@ func TestProblemFromJSON(t *testing.T) {
 			)
 		}
 	})
+}
+
+type testResponseWriter struct {
+	*strings.Builder
+	header http.Header
+	Status int
+}
+
+func (w testResponseWriter) Header() http.Header {
+	return w.header
+}
+
+func (w *testResponseWriter) WriteHeader(statusCode int) {
+	w.Status = statusCode
+}
+
+func TestWithProblem(t *testing.T) {
+	w := testResponseWriter{
+		Builder: &strings.Builder{},
+		header: make(http.Header, 0),
+	}
+	p := purchaseProblem{
+		BaseProblem: BaseProblem{
+			Type: "https://example.io/problems/out-of-credit",
+			Title: "You do not have enough credit.",
+			Detail: "Your current balance is 30, but that costs 50.",
+			Status: 418,
+			Instance: "/account/12345/msgs/abc",
+		},
+		Balance: 30,
+		Accounts: []string{
+			"/account/12345",
+			"/account/67890",
+		},
+	}
+	WithProblem(&w, p)
+
+	if w.Status != p.Status {
+		t.Errorf("response status code did not match expected. received %d, expected %d", w.Status, p.Status)
+	}
+
+	expected := `{"type":"https://example.io/problems/out-of-credit","title":"You do not have enough credit.",` +
+			`"status":418,"detail":"Your current balance is 30, but that costs 50.",` + 
+			`"instance":"/account/12345/msgs/abc",` +
+			`"balance":30,"accounts":["/account/12345","/account/67890"]}`
+	if result := w.String(); result != expected {
+		t.Errorf(
+			"problem did not serialize successfully.\nReceived: %#v\nExpected: %#v\n",
+			result,
+			expected,
+		)
+	}
 }
